c430fb4b35f7edfa4316e60ae15ebd7f0a7200f8
Trần Nhật An <trannhatandeptraibodoiqua@gmail.com>
2026-04-02 20:41:01 +0700

    Update TH12

---
+++ TH12_FragmentDong/app/src/main/java/tna/cntt2/th12_fragmentdong/MainActivity.java
@@ -0,0 +1,32 @@
+package tna.cntt2.th12_fragmentdong;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                        .add(R.id.frameContentHolder, new ContentFragment())
+                        .add(R.id.frameFooterHolder, new FooterFragment())
+                        .commit();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
