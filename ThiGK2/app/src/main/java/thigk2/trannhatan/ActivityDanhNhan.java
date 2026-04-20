433c2d91186b6bb8685ce6e1aa3d248ebcc35ed5
Trần Nhật An <trannhatandeptraibodoiqua@gmail.com>
2026-04-20 08:32:03 +0700

    LẦN THỨ 6

--- ThiGK2/app/src/main/java/thigk2/trannhatan/ActivityDanhNhan.java
+++ ThiGK2/app/src/main/java/thigk2/trannhatan/ActivityDanhNhan.java
@@ -1,24 +1,67 @@
 package thigk2.trannhatan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ActivityDanhNhan extends AppCompatActivity {
-
+    ChiTietDanhNhanAdapter danhNhanAdapter;
+    ArrayList<ChiTietDanhNhanActivity> recyclerViewDatas;
+    RecyclerView recyclerViewDanhNhan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_danh_nhan);
+
+        //3
+        recyclerViewDatas = getDataForRecyclerView();
+        //4
+        recyclerViewDanhNhan = findViewById(R.id.recyclerDanhNhan);
+        //5
+        //RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        //recyclerViewLandscape.setLayoutManager(layoutLinear);
+        //RecyclerView.LayoutManager layoutLinearHorizonal = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        //recyclerViewLandscape.setLayoutManager(layoutLinearHorizonal);
+        RecyclerView.LayoutManager layoutGrid = new GridLayoutManager(this, 1);
+        recyclerViewDanhNhan.setLayoutManager(layoutGrid);
+        //6
+        danhNhanAdapter = new ChiTietDanhNhanAdapter(this, recyclerViewDatas);
+        //7
+        recyclerViewDanhNhan.setAdapter(danhNhanAdapter);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    ArrayList<ChiTietDanhNhanActivity> getDataForRecyclerView() {
+        ArrayList<ChiTietDanhNhanActivity> dsDuLieu = new ArrayList<ChiTietDanhNhanActivity>();
+
+        dsDuLieu.add(new ChiTietDanhNhanActivity("an", "Trần Nhật An"," Nha Trang"));
+        dsDuLieu.add(new ChiTietDanhNhanActivity("", "Lê Hồng Phong","Hà Nội"));
+        dsDuLieu.add(new ChiTietDanhNhanActivity("", "Nguyễn Trãi","Hồ Chí Minh"));
+
+        return dsDuLieu;
+    }
+
+    public void QuayVe (View v) {
+        //tao doi tuong intent
+        //Tham số thứ 2 của ham tạo này, là tên Activity ( màn hình ) ta muốn chuyển sang
+        Intent iManHinhCHINH = new Intent(this, MainActivity.class);
+        //Thuc hien chuyen
+        startActivity(iManHinhCHINH);
     }
 }
--- ThiGK2/app/src/main/java/thigk2/trannhatan/ChiTietDanhNhanAdapter.java
+++ ThiGK2/app/src/main/java/thigk2/trannhatan/ChiTietDanhNhanAdapter.java
@@ -1,4 +1,80 @@
 package thigk2.trannhatan;
 
-public class ChiTietDanhNhanAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+public class ChiTietDanhNhanAdapter extends RecyclerView.Adapter<ChiTietDanhNhanAdapter.ItemDanhNhanHolder>{
+    Context context;
+    ArrayList<ChiTietDanhNhanActivity> lstData;
+
+    public ChiTietDanhNhanAdapter(Context context, ArrayList<ChiTietDanhNhanActivity> lstData) {
+        this.context = context;
+        this.lstData = lstData;
+    }
+
+    @NonNull
+    @Override
+    public ItemDanhNhanHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View vItem = cai_bom.inflate(R.layout.item_danhnhan, parent, false);
+        ItemDanhNhanHolder viewholderCreated = new ItemDanhNhanHolder(vItem);
+        return viewholderCreated;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemDanhNhanHolder holder, int position) {
+        // Lấy đối tượng hiển thị
+        ChiTietDanhNhanActivity baiThuocHienThi = lstData.get(position);
+        // Trích thông tin
+        String caption = baiThuocHienThi.getDanhNhanCation();
+        String tenFileAnh = baiThuocHienThi.getBaiThuocImageFileName();
+        String QueQuan = baiThuocHienThi.getQueQuan();
+        // Đặt vào các trường thông tin của holder
+        holder.tvCation.setText(caption);
+        holder.tvThoiGian.setText(QueQuan);
+        // Đặt ảnh
+        String packageName = holder.itemView.getContext().getPackageName();
+        int imageID = holder.itemView.getResources().getIdentifier(tenFileAnh, "mipmap", packageName);
+        holder.ivBaiThuoc.setImageResource(imageID);
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstData.size();
+    }
+
+    class ItemDanhNhanHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tvCation;
+        TextView tvThoiGian;
+        ImageView ivBaiThuoc;
+        public ItemDanhNhanHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCation = itemView.findViewById(R.id.textView);
+            tvThoiGian = itemView.findViewById(R.id.textView2);
+            ivBaiThuoc = itemView.findViewById(R.id.imageViewDanhNhan);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            //Code ở đây
+            int viTriDuocClick = getAdapterPosition();
+            ChiTietDanhNhanActivity phanTuDuocClick = lstData.get(viTriDuocClick);
+            // Bóc thông tin
+            String ten = phanTuDuocClick.getDanhNhanCation();
+            String tenFile = phanTuDuocClick.getBaiThuocImageFileName();
+            // Toast tên
+            String chuoiThongBao = "Bạn vừa click vào: " + ten;
+            Toast.makeText(v.getContext(), chuoiThongBao, Toast.LENGTH_SHORT).show();
+        }
+    }
 }
