110bf8896857a1d31c6b0746fc832fd29abbd6fd
Trần Nhật An <trannhatandeptraibodoiqua@gmail.com>
2026-05-06 08:45:42 +0700

    Phần 2

--- ViecCanLam/app/src/main/java/tna/cntt2/vieccanlam/MainActivity.java
+++ ViecCanLam/app/src/main/java/tna/cntt2/vieccanlam/MainActivity.java
@@ -1,20 +1,54 @@
 package tna.cntt2.vieccanlam;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<TASKS> lstVCL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //Tạo kết nối đến CSDL
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference("TASKS");
+        //Lắng nghe và xử lý
+        lstVCL = new ArrayList<TASKS>();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Lấy dữ liệu từ biến snapshot, đưa vào 1 biến danh sách để xử lý
+                for(DataSnapshot obj : snapshot.getChildren()) {
+                    TASKS task = obj.getValue(TASKS.class);
+                    lstVCL.add(task);
+                    Log.w("VCL app", "Tên viêc cần làm : " + task.getName());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- ViecCanLam/app/src/main/java/tna/cntt2/vieccanlam/TASKS.java
+++ ViecCanLam/app/src/main/java/tna/cntt2/vieccanlam/TASKS.java
@@ -10,6 +10,8 @@ public class TASKS implements Serializable {
     String message;
     String priority;
 
+    public TASKS() {
+    }
 
     public TASKS(String name, String date, String message, String priority) {
         this.name = name;
@@ -18,45 +20,25 @@ public class TASKS implements Serializable {
         this.priority = priority;
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
 
-    public String getDate() {
-        return date;
-    }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
+    public String getPriority() { return priority; }
+    public void setPriority(String priority) { this.priority = priority; }
 
     public HashMap<String, String> toFirebaseObject(){
-        HashMap<String, String> taskObject = new HashMap<String, String>();
+        HashMap<String, String> taskObject = new HashMap<>();
         taskObject.put("name", name);
         taskObject.put("date", date);
         taskObject.put("message", message);
         taskObject.put("priority", priority);
         return taskObject;
     }
-
 }
+
